2b34c79c09dfc8363e4800174f8d9e700a522473
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-08-19 12:12:53 +0200

    feat: store thumbnail URL and media type in video metadata

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
-import { getBearerToken, validateJWT } from "../auth";
-import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
-import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import type { ApiConfig } from "../config";
+import { getVideo, updateVideo } from "../db/videos";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { respondWithJSON } from "./json";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -11,6 +11,8 @@ type Thumbnail = {
 };
 
 const videoThumbnails: Map<string, Thumbnail> = new Map();
+const MAX_UPLOAD_SIZE = 10 * 1 << 20;
+const thumbnailWebKey = "thumbnail";
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -47,7 +49,26 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const imageData = formData.get(thumbnailWebKey)
+  if (!(imageData instanceof File)) {
+    throw new BadRequestError("thumbnail is no file");
+  }
+  if (imageData.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(`thumbnail file is too large (maximum size is ${MAX_UPLOAD_SIZE} bytes)`);
+  }
+  const mediaType = imageData.type;
+  const buffer = await imageData.arrayBuffer();
+  const videoMetadata = getVideo(cfg.db, videoId);
+  if (!videoMetadata) {
+    throw new NotFoundError("Video not found");
+  }
+  if (videoMetadata?.userID !== userID) {
+    throw new UserForbiddenError("creator of the video isn't the currently logged in user");
+  }
+  videoThumbnails.set(videoId, { data: buffer, mediaType: mediaType },);
+  videoMetadata.thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  updateVideo(cfg.db, videoMetadata);
 
-  return respondWithJSON(200, null);
+  return respondWithJSON(200, videoMetadata);
 }
